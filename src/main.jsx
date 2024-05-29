6ad344fa3c6692a8f5e50990dcf501ed2ce7fa3e
Benja Aguilar <benjaaguilar02@gmail.com>
2024-05-29 17:05:57 -0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/header.jsx";
 import MainContainer from "./components/mainContainer.jsx";
 import "./css/index.css";
+import "./css/forms.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

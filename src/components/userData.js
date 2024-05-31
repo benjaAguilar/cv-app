1fcd2b3f1d24b67caddf47967a41fbc0317cb68f
Benja Aguilar <benjaaguilar02@gmail.com>
2024-05-31 17:03:52 -0300

    feat: obj userData that stores the user input data

---
+++ src/components/userData.js
@@ -0,0 +1,13 @@
+export const userData = {
+    name: "",
+    surename: "",
+    email: "",
+    phone: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    summary: "",
+    skills: [],
+    work: [],
+    education: [],
+};
